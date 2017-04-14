a3efa75404969bbd07cb01a8d201795ad711cefb
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-04-15 00:46:18 +0200

    Configure postgres credentials for test env

--- apps/user_app/config/test.exs
+++ apps/user_app/config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :user_app, UserApp.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "developer",
+  password: "developer",
   database: "user_app_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
